3682064faaec82c82527d8877f1a37b4bb3cd398
mantaray0 <hello@mantaray0.dev>
2025-07-10 10:12:32 +0200

    formatting

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -3,21 +3,21 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('../src', import.meta.url)),
-		},
-	},
-	server: {
-		port: 3001,
-	},
-	test: {
-		environment: 'jsdom',
-		setupFiles: ['./vitest.setup.ts'],
-		globals: true,
-		coverage: {
-			reporter: ['text', 'html']
-		}
-	}
-}); 
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('../src', import.meta.url))
+        }
+    },
+    server: {
+        port: 3001
+    },
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./vitest.setup.ts'],
+        globals: true,
+        coverage: {
+            reporter: ['text', 'html']
+        }
+    }
+});
